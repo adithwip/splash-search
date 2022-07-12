d978b049db7f2cf73970c4b01056d5c5c62c59d9
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2022-07-12 13:05:16 +0700

    feat: add download button functionality and refactor component

---
+++ src/containers/HomePage/HomePage.tsx
@@ -0,0 +1,122 @@
+import { useState } from "react";
+import Image from "next/image";
+import {
+  TextInput,
+  SimpleGrid,
+  Box,
+  Text,
+  ActionIcon,
+  Avatar,
+  Stack,
+  Group,
+} from "@mantine/core";
+import { useDebouncedValue } from "@mantine/hooks";
+import { Search, Heart, Download } from "tabler-icons-react";
+
+import Layout from "components/Layout";
+import { useSearch } from "hooks/useSearch";
+import useStyles from "./styles";
+
+const HomePage = () => {
+  const [value, setValue] = useState("");
+  const [debouncedValue] = useDebouncedValue(value, 1000);
+  const { data, isLoading, isFetching, isError } = useSearch(debouncedValue);
+
+  const { classes } = useStyles();
+
+  return (
+    <Layout>
+      <TextInput
+        sx={() => ({
+          marginTop: "2rem",
+        })}
+        variant="filled"
+        radius="xl"
+        size="md"
+        placeholder="Search image here..."
+        icon={<Search size={14} />}
+        onChange={(event) => setValue(event.currentTarget.value)}
+      />
+
+      <SimpleGrid
+        cols={3}
+        my="xl"
+        breakpoints={[
+          { maxWidth: 980, cols: 3, spacing: "md" },
+          { maxWidth: 755, cols: 2, spacing: "sm" },
+          { maxWidth: 600, cols: 1, spacing: "sm" },
+        ]}
+      >
+        {data?.response?.results.map((res) => {
+          const {
+            id,
+            urls: { regular },
+            alt_description,
+            blur_hash,
+            color,
+            links: { download },
+            user: {
+              username,
+              portfolio_url,
+              profile_image: { medium: profileImage },
+            },
+          } = res;
+
+          return (
+            <Box key={id} className={classes.image_wrapper}>
+              <Box className={classes.hover_wrapper}>
+                <Box className={classes.favorite_action_wrapper}>
+                  <ActionIcon
+                    component="button"
+                    variant="filled"
+                    className={classes.favorite_action_button}
+                  >
+                    <Heart size={16} color="black" />
+                  </ActionIcon>
+                </Box>
+
+                <Group className={classes.users_and_download_wrapper}>
+                  <Group spacing="xs">
+                    <Avatar
+                      src={profileImage}
+                      radius="xl"
+                      alt={username + " avatar"}
+                    />
+
+                    <Text size="sm" color="dark" weight={500}>
+                      {username}
+                    </Text>
+                  </Group>
+
+                  <ActionIcon
+                    component="a"
+                    href={download}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    variant="outline"
+                  >
+                    <Download size={16} color="black" />
+                  </ActionIcon>
+                </Group>
+              </Box>
+              <Image
+                src={regular}
+                alt={alt_description as string}
+                placeholder="blur"
+                blurDataURL={blur_hash as string}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                style={{
+                  backgroundColor: color as string,
+                }}
+              />
+            </Box>
+          );
+        })}
+      </SimpleGrid>
+    </Layout>
+  );
+};
+
+export default HomePage;
---
+++ src/containers/HomePage/styles.ts
@@ -0,0 +1,48 @@
+import { createStyles } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  image_wrapper: {
+    position: "relative",
+    minWidth: "33%",
+    height: 400,
+  },
+
+  hover_wrapper: {
+    position: "absolute",
+    display: "hidden",
+    opacity: 0,
+    zIndex: 10,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    top: 0,
+    padding: "16px",
+
+    "&:hover": {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between",
+
+      opacity: 1,
+    },
+  },
+
+  favorite_action_wrapper: {
+    display: "flex", justifyContent: "flex-end"
+  },
+
+  favorite_action_button: {
+    backgroundColor: "white",
+    "&:hover": { backgroundColor: theme.colors.gray[0] },
+  },
+
+  users_and_download_wrapper: {
+    justifyContent: 'space-between',
+
+    padding: "8px",
+    borderRadius: "6px",
+    backgroundColor: theme.colors.gray[0],
+  }
+}));
+
+export default useStyles
